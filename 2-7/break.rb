24c463541d5f13cd348e393446f12e8ceb1ea3e0
EC2 Default User <ec2-user@ip-172-31-5-110.ec2.internal>
2021-01-11 08:19:31 +0000

    [Finish] 2-7

---
+++ 2-7/break.rb
@@ -0,0 +1,9 @@
+i = 1
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break  # iが5になると繰り返しから抜ける
+  end
+  puts i
+  i += 1
+end
---
+++ 2-7/dice.rb
@@ -0,0 +1,5 @@
+dice = 0
+while dice != 6 do
+  dice = rand(1..6)
+  puts dice
+end
